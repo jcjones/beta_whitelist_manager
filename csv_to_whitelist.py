baee82b1d6270c02c36aae3d1325972d43edb17a
J.C. Jones <jjones@mozilla.com>
2015-10-26 18:39:13 -0700

    Initial

---
+++ csv_to_whitelist.py
@@ -0,0 +1,244 @@
+#!/usr/bin/python
+"""
+Convert a CSV file into our Beta Whitelist format
+"""
+
+import csv, sys, os, re, time, random
+from jinja2 import Template
+
+import malicious_url_check
+import argparse
+import smtplib
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# The heading values in the header row for the columns
+COL_DOMAIN = "What domain(s) would you like to get a certificate for?"
+COL_EMAIL = "What email address can we contact you at when we're ready for you?"
+
+TEMPLATE_DIR = "{0}/templates".format(os.path.dirname(os.path.abspath(__file__)))
+DOMAIN_PATTERN = re.compile("^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,18}$")
+
+class DomainEntry(object):
+  def __init__(self, domain="", email=""):
+    self.domain = domain
+    self.email = email
+    self.problems = None
+
+  def __repr__(self):
+    msg = "domain={0} email={1}".format(self.domain, self.email)
+
+    if self.problems:
+      msg = "{0} problems={1}".format(msg, self.problems)
+
+    return msg
+
+class DummyFile(object):
+    def write(self, x): pass
+
+class DomainTester(object):
+  def __init__(self):
+    self.useGoogle = True
+    self.domainList = {}
+    self.emailList = {}
+    self.invalidList = []
+    self.malList = []
+
+  def setUseGoogle(self, value):
+    self.useGoogle = value
+
+  def checkAndAdd(self, domainEntry):
+    problems=[]
+    if self.useGoogle:
+      problems = malicious_url_check.malCheck(domainEntry.domain, writer=DummyFile())
+      if problems:
+        domainEntry.problems = problems
+        self.malList.append(domainEntry)
+
+    if not problems:
+      if domainEntry.email not in self.emailList:
+        self.emailList[domainEntry.email] = set()
+
+      self.emailList[domainEntry.email].add(domainEntry.domain)
+      self.domainList[domainEntry.domain] = domainEntry.email
+
+      print(domainEntry)
+
+    return problems
+
+  def processEntry(self, domains=[], email=""):
+    for domain in re.split('[,; ]', domains):
+      domain = domain.lower()
+      domain = re.sub("[ ]*(http|https)://", "", domain)
+      domain = re.sub("\*\.", "", domain)
+      domain = domain.strip()
+
+      if len(domain) < 2:
+        continue
+
+      domainEntry = DomainEntry(domain=domain, email=email)
+
+      if not DOMAIN_PATTERN.match(domain):
+        print ("Invalid: {0}".format(domainEntry))
+        self.invalidList.append(domainEntry)
+        continue
+
+      problems = self.checkAndAdd(domainEntry)
+      if problems:
+        continue
+
+      if not "www." in domain:
+        domainWww = "www.{0}".format(domain)
+        domainEntryWww = DomainEntry(domain=domainWww, email=email)
+        self.checkAndAdd(domainEntryWww)
+      else:
+        domainNaked = domain.lstrip("www.")
+        domainEntryNaked = DomainEntry(domain=domainNaked, email=email)
+        self.checkAndAdd(domainEntryNaked)
+
+  def listByDomain(self):
+    return self.domainList
+
+  def listByEmail(self):
+    return self.emailList
+
+  def listInvalid(self):
+    return self.invalidList
+
+  def listMalicious(self):
+    return self.malList
+
+def make_messageId():
+  timeval = time.time()
+  utcdate = time.strftime('%Y%m%d%H%M%S', time.gmtime(timeval))
+  pid = os.getpid()
+  randint = random.randrange(100000)
+  idstring = "betaprogram@letsencrypt.org"
+  return "<{0}.{1}.{2}.{3}>".format(utcdate, pid, randint, idstring)
+
+def sendEmail(contents):
+  from jinja2 import Environment, FileSystemLoader
+
+  env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+  txtTemplate = env.get_template('beta.txt.j2')
+  htmlTemplate = env.get_template('beta.html.j2')
+
+  addrFrom = "Let's Encrypt Beta <betaprogram@letsencrypt.org>"
+  addrTo = contents['email']
+
+  # mailServer = "10.0.12.40"
+  mailServer = "10.0.32.40"
+
+  msgRoot = MIMEMultipart()
+  msgRoot['Subject'] = "Let's Encrypt Closed Beta Invite"
+  msgRoot['From'] = addrFrom
+  msgRoot['To'] = addrTo
+  msgRoot['Message-Id'] = make_messageId()
+  msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+  msgAlternative = MIMEMultipart('alternative')
+  msgRoot.attach(msgAlternative)
+
+  msgText = MIMEText(txtTemplate.render(contents))
+  msgAlternative.attach(msgText)
+
+  msgText = MIMEText(htmlTemplate.render(contents), 'html')
+  msgAlternative.attach(msgText)
+
+  s = smtplib.SMTP(mailServer)
+  s.sendmail(addrFrom, [addrTo], msgRoot.as_string())
+  s.quit()
+  print("Email sent to {0}".format(addrTo))
+
+def processCSV(args):
+  tester = DomainTester()
+  tester.setUseGoogle(not args.noGoogle)
+
+  lineCount = 0
+  lineLimit = args.limit
+  lineOffset = args.offset
+  emailSent = 0
+
+  with open(args.csv, "r") as csvFile:
+    reader = csv.DictReader(csvFile)
+    for row in reader:
+      if lineOffset and lineOffset > 0:
+        lineOffset -= 1
+        continue
+
+      tester.processEntry(domains=row[COL_DOMAIN], email=row[COL_EMAIL])
+
+      lineCount += 1
+
+      if lineLimit:
+        lineLimit -= 1
+        if lineLimit < 1:
+          break
+
+    if args.out:
+      with open(args.out, "w") as outFile:
+        for domain, email in tester.listByDomain().iteritems():
+          outFile.write('- "{0}" # {1}\n'.format(domain.strip(), email))
+
+    if args.email:
+
+      for email, domains in tester.listByEmail().iteritems():
+
+        # Email override
+        if args.emailOverride:
+          email = "jcjones@letsencrypt.org"
+
+        sendEmail({
+          "domains": domains,
+          "email": email
+        })
+
+        emailSent += 1
+
+    if args.verbosity > 1:
+      for domain in tester.listInvalid():
+        print("Invalid: {0}".format(domain))
+
+    if args.verbosity > 0:
+      for domain in tester.listMalicious():
+        print("Malicious: {0}".format(domain))
+
+    print("Processed {entryCount} rows from limit {limit} offset {offset}. "
+      "This was {domainCount} domains and {emailCount} email addresses. "
+      "{invalidCount} were invalid, {malCount} were flagged malicious. "
+      "Sent {emailSent} emails.".format(
+        entryCount=lineCount, limit=args.limit,
+        offset=args.offset, emailSent=emailSent,
+        domainCount=len(tester.listByDomain()),
+        emailCount=len(tester.listByEmail()),
+        invalidCount=len(tester.listInvalid()),
+        malCount=len(tester.listMalicious())
+      ))
+
+
+def main():
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument("-v", dest='verbosity', help="Increase verbosity", action='count')
+  parser.add_argument("--email", help="Enable email", action='store_true')
+  parser.add_argument("--emailOverride", help="Override", action='store_true')
+  parser.add_argument("--noGoogle", help="Disable Google Safebrowsing", action='store_true')
+
+  parser.add_argument("--update", help="Update Safebrowsing", action='store_true')
+  parser.add_argument("--csv", help="CSV File")
+
+  parser.add_argument("--out", help="YAML Fragment File")
+  parser.add_argument("--offset", help="Skip rows in the CSV", type=int)
+  parser.add_argument("--limit", help="Number of rows to process",  type=int)
+  args = parser.parse_args()
+
+  if args.csv:
+    malicious_url_check.loadLists()
+    processCSV(args)
+
+  if args.update:
+    print ("Updating Safebrowsing...")
+    malicious_url_check.updateSafebrowsing()
+
+if __name__ == "__main__":
+ main()
---
+++ malicious_url_check.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+import requests, os, time, sys, zipfile
+from StringIO import StringIO
+
+try:
+  from gglsbl import SafeBrowsingList
+except:
+  print("You need to 'pip install gglsbl'")
+  sys.exit(1)
+
+try:
+  import tldextract
+except:
+  print("You need to 'pip install tldextract'")
+  sys.exit(1)
+
+
+safebrowsing_token = 'AIzaSyBKlevd7lUJpEq0XGnvaojrmS9OJqWY6YA'
+isc_url = 'https://isc.sans.edu/feeds/suspiciousdomains_Low.txt'
+topmillion_url = 'http://s3.amazonaws.com/alexa-static/top-1m.csv.zip'
+
+safebrowsing_db = os.environ['HOME'] + '/Downloads/safebrowsing.db'
+suspect_file = os.environ['HOME'] + '/Downloads/suspiciousdomains_Low.txt'
+topthousand_file = os.environ['HOME'] + '/Downloads/alexa_1000.csv'
+
+safebrowsing_bootstrap = not os.path.exists(safebrowsing_db) or (os.path.getsize(safebrowsing_db) < 10000)
+
+# Be sure to occasionally run sbl.update_hash_prefix_cache()
+sbl = SafeBrowsingList(safebrowsing_token, db_path=safebrowsing_db)
+
+ISC_LIST=[]
+ALEXA_LIST=[]
+
+def loadLists(writer=sys.stdout):
+  if isStale(suspect_file):
+    print >> writer, "Updating ISC Suspicious Domains..."
+    new_file = requests.get(isc_url)
+    with open(suspect_file, 'w') as sf_buffer:
+      sf_buffer.write(new_file.content)
+
+  if isStale(safebrowsing_db):
+    print >> writer, "Updating Google Safebrowsing DB..."
+    updateSafebrowsing()
+
+  if isStale(topthousand_file, maxTime=2629743):
+    print >> writer, "Updating Alexa Top 1000..."
+    new_file = requests.get(topmillion_url)
+    with zipfile.ZipFile(StringIO(new_file.content), 'r') as zipData:
+      with zipData.open('top-1m.csv', 'r') as oneMil:
+        with open(topthousand_file, 'w') as topThousand:
+          for i in range(0,1000):
+            topThousand.write(oneMil.readline())
+
+  for sf_read in open(suspect_file):
+    badDomain = tldextract.extract(sf_read)
+    ISC_LIST.append(badDomain)
+
+  for topthousand_read in open(topthousand_file):
+    cleaned_line = topthousand_read.split(",")[1].strip()
+    valuableDomain = tldextract.extract(cleaned_line)
+    ALEXA_LIST.append(valuableDomain)
+
+
+def updateSafebrowsing():
+  sbl.update_hash_prefix_cache()
+
+def isStale(filePath, maxTime=86400):
+  """
+  Check if file is older than maxTime and refresh if needed
+  """
+  return (not os.path.exists(filePath)) or (time.time() - os.path.getmtime(filePath)) > maxTime
+
+def malCheck(in_url, writer=sys.stdout):
+  problems=[]
+
+  if not ALEXA_LIST or not ISC_LIST:
+    raise Exception("You must call loadLists() first!")
+
+  check_url = tldextract.extract(in_url)
+  if len(check_url.suffix) < 2:
+    problems.append("Invalid TLD")
+    return problems
+
+  print >> writer, "Checking maliciousness of {0}".format(check_url)
+
+  # Check if URL is in suspicious domains list from SAN ISC
+  for sf_read in open(suspect_file):
+    sf = tldextract.extract(sf_read)
+
+    if sf.registered_domain == check_url.registered_domain:
+      problems.append("Matches ISC suspicious domain {0}".format(sf))
+
+  for topthousand_read in open(topthousand_file):
+    cleaned_line = topthousand_read.split(",")[1].strip()
+    valuableDomain = tldextract.extract(cleaned_line)
+
+    if valuableDomain.registered_domain == check_url.registered_domain:
+      problems.append("Matches Alexa Top 1000 domain {0}".format(valuableDomain))
+
+  # ISC Suspicious Domains
+  for badDomain in ISC_LIST:
+    if badDomain.registered_domain == check_url.registered_domain:
+      problems.append("Matches ISC suspicious domain {0}".format(badDomain))
+
+  # Alexa Top 1000
+  for valuableDomain in ALEXA_LIST:
+    if valuableDomain.registered_domain == check_url.registered_domain:
+      problems.append("Matches Alexa Top 1000 domain {0}".format(valuableDomain))
+
+  # Check against Google Safe Browsing API
+  sbr = sbl.lookup_url(check_url)
+  if sbr:
+    problems.extend(sbr)
+
+  return problems
+
+if __name__ == "__main__":
+  if safebrowsing_bootstrap:
+    print("Initial download of SafeBrowsing DB... this will take a few minutes.")
+    updateSafebrowsing()
+
+  loadLists()
+
+  for url in sys.argv[1:]:
+    results = malCheck(url)
+    for res in results:
+      print("Malicious: {0}".format(results))
